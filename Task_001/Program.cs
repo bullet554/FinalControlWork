0c09d59f03f9d0a1bf6dabd3fdf614f2c763139d
Ilya <ilya.karpov.93@mail.ru>
2024-09-03 13:59:50 +0300

    Creat a array and print function

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿void Main()
+{
+    string[] array = { "Hello", "2", "world", ":-)" };
+
+
+    PrintArray(array);
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write(array[i] + "\t");
+}
+
+
+Main();
