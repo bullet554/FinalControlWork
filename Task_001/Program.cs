badf298021b452fac789d287e5bf7533d4109cf9
Ilya <ilya.karpov.93@mail.ru>
2024-09-03 14:27:30 +0300

    Creat a function CreartigANewArray

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -6,6 +6,18 @@
     PrintArray(array);
 }
 
+void CreatingANewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+}
+
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
